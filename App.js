38b9e110ef098782fdeabab1d0168ec94ae4e9c1
cehmenang <heaven.cehwin12@gmail.com>
2023-06-07 08:25:24 +0700

    Menginisiasi Project Personal

---
+++ App.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+class App{
+    constructor(){
+        this.app = express()
+        this.port = process.env.PORT || 8000
+        this.middleware()
+        this.connection()
+    }
+    middleware(){
+        this.app.use(express.json())
+        this.app.use(express.urlencoded({ extended: true }))
+        this.app.get('/about', (req, res)=>res.send(`Halo, ini dari halaman about!`))
+        this.app.use('*', (req, res)=>res.send(`Halo, ini adalah halaman default!`))
+    }
+    connection(){
+        this.app.listen(this.port, ()=>console.info(`Berhasil Menjalankan Server Pada http://localhost:${this.port}`))   
+    }
+}
+
+const app = new App().app
